282988caa951d9a129884d979219c32f5b0413f0
somiew <somiew@hotmail.com>
2021-02-24 18:02:54 +0100

    base functionality works

--- autorotoUI.py
+++ autorotoUI.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
+# python 3.8.7, Windows 10, 64 bit
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 import FindMattes as fm
-
+from os import listdir
 
 class Ui_MainWindow(QtWidgets.QWidget):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(425, 263)
+        MainWindow.resize(452, 276)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.formLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.formLayoutWidget.setGeometry(QtCore.QRect(10, 10, 381, 22))
+        self.formLayoutWidget.setGeometry(QtCore.QRect(10, 10, 391, 22))
         self.formLayoutWidget.setObjectName("formLayoutWidget")
         self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
         self.formLayout.setContentsMargins(0, 0, 0, 0)
@@ -25,7 +26,7 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.inputEdit.setObjectName("inputEdit")
         self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.inputEdit)
         self.formLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
-        self.formLayoutWidget_2.setGeometry(QtCore.QRect(10, 190, 381, 22))
+        self.formLayoutWidget_2.setGeometry(QtCore.QRect(10, 200, 391, 22))
         self.formLayoutWidget_2.setObjectName("formLayoutWidget_2")
         self.formLayout_5 = QtWidgets.QFormLayout(self.formLayoutWidget_2)
         self.formLayout_5.setContentsMargins(0, 0, 0, 0)
@@ -37,7 +38,7 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.outputLabel.setObjectName("outputLabel")
         self.formLayout_5.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.outputLabel)
         self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(10, 40, 411, 111))
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(10, 40, 431, 111))
         self.gridLayoutWidget.setObjectName("gridLayoutWidget")
         self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
@@ -103,7 +104,7 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.cow.setObjectName("cow")
         self.gridLayout.addWidget(self.cow, 1, 4, 1, 1)
         self.formLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
-        self.formLayoutWidget_3.setGeometry(QtCore.QRect(270, 160, 151, 22))
+        self.formLayoutWidget_3.setGeometry(QtCore.QRect(270, 170, 171, 22))
         self.formLayoutWidget_3.setObjectName("formLayoutWidget_3")
         self.formLayout_4 = QtWidgets.QFormLayout(self.formLayoutWidget_3)
         self.formLayout_4.setContentsMargins(0, 0, 0, 0)
@@ -115,38 +116,49 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.vosLabel.setObjectName("vosLabel")
         self.formLayout_4.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.vosLabel)
         self.formLayoutWidget_4 = QtWidgets.QWidget(self.centralwidget)
-        self.formLayoutWidget_4.setGeometry(QtCore.QRect(10, 160, 121, 22))
+        self.formLayoutWidget_4.setGeometry(QtCore.QRect(10, 170, 121, 22))
         self.formLayoutWidget_4.setObjectName("formLayoutWidget_4")
         self.formLayout_2 = QtWidgets.QFormLayout(self.formLayoutWidget_4)
         self.formLayout_2.setContentsMargins(0, 0, 0, 0)
         self.formLayout_2.setObjectName("formLayout_2")
-        self.ffEdit = QtWidgets.QLineEdit(self.formLayoutWidget_4)
-        self.ffEdit.setObjectName("ffEdit")
-        self.formLayout_2.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.ffEdit)
-        self.ffLabel = QtWidgets.QLabel(self.formLayoutWidget_4)
-        self.ffLabel.setObjectName("ffLabel")
-        self.formLayout_2.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.ffLabel)
+
+        # Commented out, because first and last frame functionality wont be a thing for a while
+        # ----------
+        # self.ffEdit = QtWidgets.QLineEdit(self.formLayoutWidget_4)
+        # self.ffEdit.setObjectName("ffEdit")
+        # self.formLayout_2.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.ffEdit)
+        # self.ffLabel = QtWidgets.QLabel(self.formLayoutWidget_4)
+        # self.ffLabel.setObjectName("ffLabel")
+        # self.formLayout_2.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.ffLabel)
         self.formLayoutWidget_5 = QtWidgets.QWidget(self.centralwidget)
-        self.formLayoutWidget_5.setGeometry(QtCore.QRect(140, 160, 121, 22))
+        self.formLayoutWidget_5.setGeometry(QtCore.QRect(140, 170, 121, 22))
         self.formLayoutWidget_5.setObjectName("formLayoutWidget_5")
         self.formLayout_3 = QtWidgets.QFormLayout(self.formLayoutWidget_5)
         self.formLayout_3.setContentsMargins(0, 0, 0, 0)
         self.formLayout_3.setObjectName("formLayout_3")
-        self.lsEdit = QtWidgets.QLineEdit(self.formLayoutWidget_5)
-        self.lsEdit.setObjectName("lsEdit")
-        self.formLayout_3.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.lsEdit)
-        self.lsLabel = QtWidgets.QLabel(self.formLayoutWidget_5)
-        self.lsLabel.setObjectName("lsLabel")
-        self.formLayout_3.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.lsLabel)
+        
+        # Commented out, because first and last frame functionality wont be a thing for a while
+        # ----------
+        # self.lsEdit = QtWidgets.QLineEdit(self.formLayoutWidget_5)
+        # self.lsEdit.setObjectName("lsEdit")
+        # self.formLayout_3.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.lsEdit)
+        # self.lsLabel = QtWidgets.QLabel(self.formLayoutWidget_5)
+        # self.lsLabel.setObjectName("lsLabel")
+        # self.formLayout_3.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.lsLabel)
         self.run = QtWidgets.QPushButton(self.centralwidget)
-        self.run.setGeometry(QtCore.QRect(340, 220, 81, 23))
+        self.run.setGeometry(QtCore.QRect(340, 230, 81, 23))
         self.run.setObjectName("run")
         self.inputButton = QtWidgets.QToolButton(self.centralwidget)
-        self.inputButton.setGeometry(QtCore.QRect(390, 10, 31, 21))
+        self.inputButton.setGeometry(QtCore.QRect(410, 10, 31, 21))
         self.inputButton.setObjectName("inputButton")
         self.outputButton = QtWidgets.QToolButton(self.centralwidget)
-        self.outputButton.setGeometry(QtCore.QRect(390, 190, 31, 21))
+        self.outputButton.setGeometry(QtCore.QRect(410, 200, 31, 21))
         self.outputButton.setObjectName("outputButton")
+        self.line = QtWidgets.QFrame(self.centralwidget)
+        self.line.setGeometry(QtCore.QRect(10, 151, 431, 20))
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -154,7 +166,8 @@ class Ui_MainWindow(QtWidgets.QWidget):
 
         # buttons
         self.run.clicked.connect(lambda: self.create_matte())
-        self.inputButton.clicked.connect(lambda: self.open_finder())
+        self.inputButton.clicked.connect(lambda: self.open_input_finder())
+        self.outputButton.clicked.connect(lambda: self.open_output_finder())
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -226,31 +239,45 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.cow.setStatusTip(_translate("MainWindow", "(64, 128, 0)"))
         self.cow.setText(_translate("MainWindow", "Cow"))
         self.vosLabel.setText(_translate("MainWindow", "Vertical output size:"))
-        self.ffLabel.setText(_translate("MainWindow", "First frame:"))
-        self.lsLabel.setText(_translate("MainWindow", "Last frame:"))
+        #self.ffLabel.setText(_translate("MainWindow", "First frame:"))
+        #self.lsLabel.setText(_translate("MainWindow", "Last frame:"))
         self.run.setText(_translate("MainWindow", "Run"))
         self.inputButton.setText(_translate("MainWindow", "..."))
         self.outputButton.setText(_translate("MainWindow", "..."))
 
-    # My functions
+        # My functions
     def create_matte(self):
         folderInput = self.inputEdit.text()
         folderOutput = self.outputEdit.text()
         vSize = self.vosEdit.text()
         vSize = int(vSize)
-        print(folderInput, folderOutput, vSize)
-        fm.createMatte(folderInput, folderOutput, vSize)
 
-    def open_finder(self):
+        # for every image in folder (but in reality every image)
+        imgList = listdir(folderInput)
+        for img in imgList:
+            inputName = folderInput + '/' + img
+
+            # find lenght til imgname ends, add _autoMatte and then add the rest of the file name back
+            imgNameEnd = img.find('.')
+            outputName = img[:imgNameEnd] + '_autoMatte' + img[imgNameEnd:]
+
+            outputName = folderOutput + '/' + outputName
+            print(inputName, outputName, vSize)
+            fm.createMatte(inputName, outputName, vSize)
+            print('done')
+
+    def open_input_finder(self):
         folderpath = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
-        print(folderpath)
+        self.inputEdit.setText(folderpath)
+        self.outputEdit.setText(folderpath)
+
+    def open_output_finder(self):
+        folderpath = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.outputEdit.setText(folderpath)
 
 
 if __name__ == "__main__":
     import sys
-
-    import os    
-    path = 'C:'
 
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
